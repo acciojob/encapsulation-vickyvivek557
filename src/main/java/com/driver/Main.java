f738ef3b078babbf6d40996e214ed340104a479c
Vivek Kumar <vickyvivek557@gmail.com>
2023-08-10 12:56:14 +0530

    1st commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj = new RWOnly();
+//      obj.name = "Vivek";
+//      System.out.println(name);
+      //Error: name has private access in com.driver.RWOnly
+        obj.setName("Vivek");
+        String name = obj.getName();
+  }
 }
